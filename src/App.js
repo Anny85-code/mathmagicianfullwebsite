f569db682ac1190b1186d3a08b85d91011600fbe
uae <aniekaudo@yahoo.com>
2022-02-08 16:44:15 +0100

    Updated css files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Calculator from './Component/Calculator';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Calculator />
     </div>
   );
 }
---
+++ src/Component/Calculator.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+const Calculator = () => {
+  const [calc, setCalc] = useState('');
+  const [result, setResult] = useState('');
+  const operator = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = (value) => {
+    if (
+      (operator.includes(value) && calc === '')
+      || (operator.includes(value) && operator.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+    if (!operator.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 9; i > 0; i -= 1) {
+      digits.push(
+        <button type="submit" onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>,
+      );
+    }
+    return digits;
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const deleteLast = () => {
+    if (calc === '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {result ? (
+            <span>
+              (
+              {result}
+              )
+            </span>
+          ) : ''}
+          &nbsp;
+          {calc || '0'}
+        </div>
+        <div className="operator-digits-container">
+          <div className="operators">
+            <button type="submit" onClick={() => updateCalc('/')}>/</button>
+            <button type="submit" onClick={() => updateCalc('*')}>*</button>
+            <button type="submit" onClick={() => updateCalc('+')}>+</button>
+            <button type="submit" onClick={() => updateCalc('-')}>-</button>
+            <button type="submit" onClick={calculate}>=</button>
+          </div>
+          <div className="digits">
+            <button type="submit" onClick={deleteLast}>A/C</button>
+            <button type="submit" onClick={deleteLast}>+/-</button>
+            <button type="submit" onClick={deleteLast}>%</button>
+            {createDigits()}
+            <button type="submit" onClick={() => updateCalc('0')}>0</button>
+            <button type="submit" onClick={() => updateCalc('.')}>.</button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
