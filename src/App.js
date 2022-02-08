26d2c41d73e1f263683cab35064efd809a38ddca
uae <aniekaudo@yahoo.com>
2022-02-08 23:15:35 +0100

    Modified readme file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Calculator from './Component/Calculator';
 import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 export default App;
