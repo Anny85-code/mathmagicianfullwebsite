aa5e1c072793bbcfe0646dd5b81f36cbca6abd55
uae <aniekaudo@yahoo.com>
2022-02-16 15:46:42 +0100

    Adding router component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Calculator from './Component/Calculator';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './Pages/Calculator';
+import Quote from './Pages/Quote';
+import Home from './Pages/Home';
+import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+
 import './index.css';
 
 const App = () => (
   <div className="App">
-    <Calculator />
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/quote" element={<Quote />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
+    <Footer />
   </div>
 );
-
 export default App;
---
+++ src/Pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote-div">
+    <p className="home-text">
+      Mathematics is not about number, equations, computations or algorithms: It
+      is about understanding- William paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
