3c2778e3bb3c692ad575db5f901fd13c39d876f7
uae <aniekaudo@yahoo.com>
2022-02-21 09:56:25 +0100

    :goal_net: Added background image
    Added background image in css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import Quote from './Pages/Quote';
 import Home from './Pages/Home';
 import Navbar from './Component/Navbar';
 import Footer from './Component/Footer';
-
 import './index.css';
 
-const App = () => (
-  <div className="App">
-    <Navbar />
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/quote" element={<Quote />} />
-      <Route path="/calculator" element={<Calculator />} />
-    </Routes>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const myStyle = {
+    backgroundImage: `linear-gradient(
+      to right,
+      rgba(218, 165, 32, 0.65),
+      rgba(238, 82, 60, 0.65)
+    ),url(${`${process.env.PUBLIC_URL}/math-image-3.jpg`})`,
+    height: '100vh',
+    fontSize: '50px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
+  return (
+    <div className="App" style={myStyle}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 export default App;
